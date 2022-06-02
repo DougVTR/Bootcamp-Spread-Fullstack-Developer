889aa1567135b267c425556e12523fc574596c15
Douglas <douglasvtr1999@gmail.com>
2022-06-02 12:47:46 -0300

    atualização arquivo meuapp react

--- React/meuapp/src/pages/Home/index.jsx
+++ React/meuapp/src/pages/Home/index.jsx
@@ -1,10 +1,41 @@
+import React from 'React';
 import './styles.css';
+import { Card } from '../../components/Card'
+const { useState } = React;
+
 export function Home() {
-  return ( 
-    <div className="app"> 
-      <h1>Quem é você?</h1>
-      <input type="text" name="nome" id="nome" placeholder="Digite seu nome..." />
+
+  const [studentName, setStudentName] = useState();
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent (e){
+
+    e.preventDefault();
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString('pt-br',{
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    };
+    setStudents(prevState => [...prevState,newStudent]);
+  }
+  
+  return (
+    <div className="app">
+      <h1>Lista de Presença</h1>
+      <form onSubmit={handleAddStudent}>
+      <input type="text" name="nome" id="nome" placeholder="Digite o nome..."
+        onChange={e => setStudentName(e.target.value)} />
       <button id='enviar'>Enviar</button>
-    </div> 
+      </form>
+
+      {
+        students.map(student => <Card nome={student.name} horas={student.time} /> )
+      }
+    </div>
+    
   )
+  
 }
